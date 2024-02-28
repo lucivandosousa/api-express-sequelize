b0a9545ee9150b5134b6b249e99929654c4ef89f
Lucivando Sousa <lucivandosousa@gmail.com>
2024-02-28 11:13:43 -0300

    feat: criar rotas do modelo pessoa

---
+++ src/routes/pessoa.route.js
@@ -0,0 +1,12 @@
+const { Router } = require("express")
+const PessoaController = require("../controllers/PessoaController.js")
+
+const pessoaRoute = Router()
+
+pessoaRoute.post("/pessoas", PessoaController.salvarPessoa)
+pessoaRoute.get("/pessoas", PessoaController.buscarPessoas)
+pessoaRoute.get("/pessoas/:id", PessoaController.buscarPessoaPorID)
+pessoaRoute.put("/pessoas/:id", PessoaController.atualizarPessoa)
+pessoaRoute.delete("/pessoas/:id", PessoaController.deletarPessoa)
+
+module.exports = pessoaRoute
