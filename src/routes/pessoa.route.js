cffe999691173d6bf68f96e2f90011ffe36027ff
Lucivando Sousa <lucivandosousa@gmail.com>
2024-03-04 15:42:42 -0300

    feat: criar rota para pegar matriculas por estudante

--- src/routes/pessoa.route.js
+++ src/routes/pessoa.route.js
@@ -8,5 +8,6 @@ pessoaRoute.get("/pessoas", PessoaController.buscarPessoas)
 pessoaRoute.get("/pessoas/:id", PessoaController.buscarPessoaPorID)
 pessoaRoute.put("/pessoas/:id", PessoaController.atualizarPessoa)
 pessoaRoute.delete("/pessoas/:id", PessoaController.deletarPessoa)
+pessoaRoute.get("/pessoas/:id/matriculas", PessoaController.buscarMatriculas)
 
 module.exports = pessoaRoute
